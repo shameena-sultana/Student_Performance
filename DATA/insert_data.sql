827ba5246032aaff5a91df8c2be81af37bcf48e4
SHAMEENA SULTANA <69394359+shameena-sultana@users.noreply.github.com>
2025-05-30 20:51:22 +0530

    Creating insert_data.sql

---
+++ DATA/insert_data.sql
@@ -0,0 +1,23 @@
+--Inserting into students
+INSERT INTO students VALUES
+(1, 'Alice', 'Female', '10A'),
+(2, 'Bob', 'Male', '10A'),
+(3, 'Charlie', 'Male', '10B');
+
+-- Inserting into Subjects
+INSERT INTO subjects VALUES
+(1, 'Math'),
+(2, 'Science'),
+(3, 'English');
+
+-- Inserting into Scores
+INSERT INTO scores VALUES
+(1, 1, 1, 88, 'Term1'),
+(2, 1, 2, 92, 'Term1'),
+(3, 1, 3, 85, 'Term1'),
+(4, 2, 1, 76, 'Term1'),
+(5, 2, 2, 79, 'Term1'),
+(6, 2, 3, 81, 'Term1'),
+(7, 3, 1, 90, 'Term1'),
+(8, 3, 2, 95, 'Term1'),
+(9, 3, 3, 89, 'Term1');
