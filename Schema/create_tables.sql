395bb0ab77f091c30b72384ee6a5c0ddf9a26660
SHAMEENA SULTANA <69394359+shameena-sultana@users.noreply.github.com>
2025-05-30 20:52:20 +0530

    Updating create_tables.sql

--- Schema/create_tables.sql
+++ Schema/create_tables.sql
@@ -1,13 +1,16 @@
+--Creating students table
 CREATE TABLE students(
   student_id INT PRIMARY KEY,
   name TEXT,
   gender TEXT,
   class TEXT);
 
+--Creating subjects table
 CREATE TABLE subjects(
   subject_id INT PRIMARY KEY,
   subject_name TEXT);
 
+--Creating scores table
 CREATE TABLE scores(
   score_id INT PRIMARY KEY,
   student_id INT,
